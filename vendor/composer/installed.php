4c03fbdf74178bd69053511dbebdce9327d5e723
davidragus <davidruizfp23@ibf.cat>
2024-11-17 13:52:35 +0100

    init: Install composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'davidruiz/tieflingstavern',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '42730e7786e85586ba808a20747ee48a899c3f78',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'davidruiz/tieflingstavern' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '42730e7786e85586ba808a20747ee48a899c3f78',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
