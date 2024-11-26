3a6ba9a5a3a24a6692d76c64bd41a93395cd0999
davidragus <davidruizfp23@ibf.cat>
2024-11-26 17:17:52 +0100

    feat: Add base for product containers and images

--- src/views/common/productContainer.php
+++ src/views/common/productContainer.php
@@ -1,6 +1,6 @@
 <a class="card product-container rounded-0 border-0 px-0">
 	<img src="<?= images('Logo') ?>" class="card-img-top rounded-0" alt="...">
-	<div class="card-body">
-		<h5 class="card-title">TESTING</h5>
+	<div class="card-body d-flex align-items-center justify-content-center px-5">
+		<h4 class="card-title text-center">TESTING</h4>
 	</div>
 </a>
