ffeb7ebe5243e990a5757153223877dc7fc91a35
davidragus <davidruizfp23@ibf.cat>
2024-11-27 17:06:07 +0100

    style: Modify categories card style

--- src/views/common/categoryContainer.php
+++ src/views/common/categoryContainer.php
@@ -1,7 +1,6 @@
-<a class="card category-container rounded-0 border-0 p-0 my-lg-3 mx-lg-2 mx-xxl-2 <?= !$params && !isset($_GET['id']) ? 'd-none' : '' ?> <?= $params && (isset($_GET['id']) && $params->getCategoryId() == $_GET['id']) ? 'd-none' : '' ?>"
+<a class="card category-container rounded-0 border-0 p-0 my-lg-3 mx-lg-2 mx-xxl-2 <?= !$params && !isset($_GET['id']) ? 'active' : '' ?> <?= $params && (isset($_GET['id']) && $params->getCategoryId() == $_GET['id']) ? 'active' : '' ?>"
 	href="<?= $params ? url("products/index/" . $params->getCategoryId()) : url("products") ?>">
-	<img src="<?= $params ? images($params->getImage()) : images('Logo') ?>" class="card-img-top rounded-0" alt="...">
-	<div class="card-body d-flex align-items-center justify-content-center px-5">
-		<h4 class="card-text text-center"><?= $params ? $params->getName() : 'All Products' ?></h4>
+	<div class="card-body d-flex align-items-center justify-content-center px-3">
+		<h4 class="card-text text-center"><?= $params ? $params->getName() : 'Show All Products' ?></h4>
 	</div>
 </a>
