b7f21e559d7eb30b3cdd9e97d74e573b63f18c12
davidragus <davidruizfp23@ibf.cat>
2024-12-09 19:35:50 +0100

    feat: Add on sale tag to product container

--- src/views/common/productContainer.php
+++ src/views/common/productContainer.php
@@ -1,6 +1,9 @@
-<a class="card product-container rounded-0 border-0 px-0 my-lg-3 mx-lg-2 mx-xxl-2">
+<a class="card product-container rounded-0 border-0 px-0 my-lg-3 mx-lg-2 mx-xxl-2 position-relative">
 	<img src="<?= images($params->getImage()) ?>" class="card-img-top rounded-0" alt="...">
 	<div class="card-body d-flex align-items-center justify-content-center px-5">
 		<h4 class="card-title text-center"><?= $params->getName() ?></h4>
 	</div>
+	<span class="on-sale-tag position-absolute text-light">
+		ON SALE
+	</span>
 </a>
