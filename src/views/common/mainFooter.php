f49d277d79c04008a92c4cbf47f847ad9048b41a
davidragus <davidruizfp23@ibf.cat>
2024-12-13 18:45:36 +0100

    feat: Add some parts of the footer

--- src/views/common/mainFooter.php
+++ src/views/common/mainFooter.php
@@ -6,5 +6,26 @@
 			href="#top">
 			<i class="bi bi-chevron-compact-up position-absolute"></i>BACK TO TOP</a>
 	</div>
-	<div class="row"></div>
+	<div class="row footer-content">
+		<div class="col-6">
+			<ul class="m-0 p-0 mb-5">
+				<li>
+					<span class="list-header pb-1 text-white">SOCIAL NETWORKS</span>
+					<ul class="m-0 px-0 pt-3 d-flex">
+						<li><a class="round-button d-flex justify-content-center align-items-center rounded-5 me-2"
+								href="#"><?= icons('facebook') ?></a></li>
+						<li><a class="round-button d-flex justify-content-center align-items-center rounded-5 me-2"
+								href="#"><?= icons('twitter-x') ?></a></li>
+						<li><a class="round-button d-flex justify-content-center align-items-center rounded-5 me-2"
+								href="#"><?= icons('instagram') ?></a></li>
+					</ul>
+				</li>
+			</ul>
+			<iframe
+				src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d726.9627887299667!2d2.1603087762745328!3d41.39455646504854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a293c3554c5b%3A0x798905e8f13b6f22!2sC.%20de%20Proven%C3%A7a%2C%20255%2C%20Eixample%2C%2008008%20Barcelona!5e0!3m2!1ses!2ses!4v1734018588069!5m2!1ses!2ses"
+				height="300" style="border:0;" allowfullscreen="" loading="lazy"
+				referrerpolicy="no-referrer-when-downgrade" class="p-0 col-12"></iframe>
+		</div>
+		<div class="col-6"></div>
+	</div>
 </div>
