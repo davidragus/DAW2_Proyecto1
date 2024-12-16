0f8eb8a2c6945f73a4583858a7968fd9b19d49a4
davidragus <davidruizfp23@ibf.cat>
2024-12-16 19:58:15 +0100

    feat: Add footer design

--- src/views/common/mainFooter.php
+++ src/views/common/mainFooter.php
@@ -6,10 +6,10 @@
 			href="#top">
 			<i class="bi bi-chevron-compact-up position-absolute"></i>BACK TO TOP</a>
 	</div>
-	<div class="row footer-content">
-		<div class="col-6">
-			<ul class="m-0 p-0 mb-5">
-				<li>
+	<div class="row footer-content h-auto">
+		<div class="col-6 p-0 pe-5">
+			<ul class="m-0 p-0">
+				<li class="mb-4">
 					<span class="list-header pb-1 text-white">SOCIAL NETWORKS</span>
 					<ul class="m-0 px-0 pt-3 d-flex">
 						<li><a class="round-button d-flex justify-content-center align-items-center rounded-5 me-2"
@@ -20,12 +20,60 @@
 								href="#"><?= icons('instagram') ?></a></li>
 					</ul>
 				</li>
+				<li>
+					<span class="list-header pb-1 text-white">OUR LOCATION</span>
+					<ul class="m-0 px-0 pt-3">
+						<li class="mb-2"><span class="text-white opacity-75">Carrer de Provença, 255,
+								08008 Barcelona, Spain</span></li>
+						<li>
+							<iframe
+								src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d726.9627887299667!2d2.1603087762745328!3d41.39455646504854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a293c3554c5b%3A0x798905e8f13b6f22!2sC.%20de%20Proven%C3%A7a%2C%20255%2C%20Eixample%2C%2008008%20Barcelona!5e0!3m2!1ses!2ses!4v1734018588069!5m2!1ses!2ses"
+								height="300" style="border:0;" allowfullscreen="" loading="lazy"
+								referrerpolicy="no-referrer-when-downgrade" class="p-0 col-12"></iframe>
+						</li>
+					</ul>
+				</li>
 			</ul>
-			<iframe
-				src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d726.9627887299667!2d2.1603087762745328!3d41.39455646504854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a293c3554c5b%3A0x798905e8f13b6f22!2sC.%20de%20Proven%C3%A7a%2C%20255%2C%20Eixample%2C%2008008%20Barcelona!5e0!3m2!1ses!2ses!4v1734018588069!5m2!1ses!2ses"
-				height="300" style="border:0;" allowfullscreen="" loading="lazy"
-				referrerpolicy="no-referrer-when-downgrade" class="p-0 col-12"></iframe>
 		</div>
-		<div class="col-6"></div>
+		<div class="col-6 p-0 ps-5">
+			<div class="row">
+				<ul class="col-4 m-0 p-0">
+					<li class="mb-4">
+						<span class="list-header pb-1 text-white">WEBSITE POLICIES</span>
+						<ul class="m-0 px-0 pt-3">
+							<li class="my-1"> <a class="sublist-link" href="">Privacy Policy</a> </li>
+							<li class="my-1"> <a class="sublist-link" href="">Cookies Policy</a> </li>
+							<li class="my-1"> <a class="sublist-link" href="">Legal Notice</a> </li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="col-4 m-0 p-0">
+					<li class="mb-4">
+						<span class="list-header pb-1 text-white">COMPANY</span>
+						<ul class="m-0 px-0 pt-3">
+							<li class="my-1"> <a class="sublist-link" href="">About</a> </li>
+							<li class="my-1"> <a class="sublist-link" href="">Careers</a> </li>
+							<li class="my-1"> <a class="sublist-link" href="">Support</a> </li>
+							<li class="my-1"> <a class="sublist-link" href="">Wizards Play Network</a> </li>
+							<li class="my-1"> <a class="sublist-link" href="">Affiliate Program Disclosure</a> </li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="col-4 m-0 p-0">
+					<li class="mb-4">
+						<span class="list-header pb-1 text-white">BRANDS</span>
+						<ul class="m-0 px-0 pt-3">
+							<li class="my-1"> <a class="sublist-link" href="">Dungeons & Dragons</a> </li>
+							<li class="my-1"> <a class="sublist-link" href="">Duel Masters</a> </li>
+							<li class="my-1"> <a class="sublist-link" href="">Magic: The Gathering</a> </li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row mt-3 py-2">
+			<span class="footer-copy text-white text-center opacity-75">© 2024 Tiefling's Tavern. Made by David Ruiz
+				Agustín</span>
+		</div>
 	</div>
 </div>
